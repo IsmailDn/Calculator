443be079b900f79a8f143aff7def3eabfaad99a1
Ismail <ismail.dindar.04@gmail.com>
2024-11-24 13:44:56 +0300

    CW1_Calculator

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Hesapmakinesi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("1. sayıyı giriniz: ");
+            var sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("2. sayıyı giriniz: ");
+            var sayi2 = Convert.ToInt32(Console.ReadLine());
+
+
+            var toplam = (sayi1 + sayi2);
+            var fark = (sayi1 - sayi2);
+            var carpım = (sayi1 * sayi2);
+            var bolme = (sayi1 / sayi2);
+
+            Console.WriteLine("Toplam : " + toplam);
+            Console.WriteLine("Fark : " + fark);
+            Console.WriteLine("Çarpım : " + carpım);
+            Console.WriteLine("Bölme : " + bolme);
+
+
+
+
+
+        }
+    }
+}
